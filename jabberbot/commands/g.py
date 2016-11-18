2b16268761f97694df5c07934a437dac16cdbb3f
Jeff <PentSec@cock.li>
2016-11-17 21:38:30 -0400

    Delete g.py

--- jabberbot/commands/g.py
+++
@@ -1,9 +0,0 @@
-from wikipedia import wikipedia
-
-def run_command(msg, *args):
-    """Busqueda en wikipedia"""
-    mtype = 'groupchat'
-    if args:
-        sh = wikipedia.summary(args)
-        return mtype, 'Hello'
-    return mtype, 'que debo buscar?'
